ecafbf265f33893ce7229906b421bc915afad3ba
Marta Kulig <bialka104b@interia.pl>
2022-11-19 16:05:37 +0100

    add input file and radiobox

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'frontend';
+  srcResult = '';
+
+  onFileSelected() {
+    const inputNode: any = document.querySelector('#file');
+
+    if (typeof FileReader !== 'undefined') {
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.srcResult = e.target.result;
+      };
+
+      reader.readAsArrayBuffer(inputNode.files[0]);
+    }
+  }
 }
