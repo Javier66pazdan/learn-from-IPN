84cd4cc261eebc760b4dbd762de080c300bf4adf
michmot12@gmail.com <michmot12@gmail.com>
2022-11-20 07:59:57 +0100

    ban words list

---
+++ backend/questionGenerator.py
@@ -0,0 +1,67 @@
+import math
+import random
+import pdfReader
+import csv
+from difflib import get_close_matches
+
+def generateDate(sentences,level):
+    question = []
+    for x in sentences:
+        for y in x.split(" "):
+            if y.isnumeric() and len(y) == 4:
+                iteration=0
+                answers = []
+                answers.append(int(y))
+                while(iteration < 3):
+                     wrong = int(y)+math.trunc(random.random()*level)
+                     if not wrong in answers:
+                        iteration+= 1
+                        answers.append(wrong)
+                question.append(
+                    {
+                        "question":x.replace(y,"_________"),
+                        "answer":int(y),
+                        "answers": answers
+                    })
+    return question
+
+def generateName(sentences,level):
+    rows=[]
+    result=[]
+    with open("imiona_polskie.csv",'r',encoding='utf-8-sig') as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            rows.append(row[0].lower())
+    for x in sentences:
+        for y in x.split(" "):
+            name = get_close_matches(y.lower(),rows, 1, 0.90)
+            if len(name) != 0:
+                answers = [y,rows[math.trunc(random.random()*50)].capitalize(),rows[math.trunc(random.random()*50)].capitalize(),rows[math.trunc(random.random()*50)].capitalize()]
+                result.append( {
+                        "question":x.replace(y,"_________"),
+                        "answer":y,
+                        "answers": answers
+                    })
+                break
+                    
+    return result
+#TODO Losowe złe wartosci i poziomy trudnosci
+def generatePlace(sentences,level):
+    rows=[]
+    result=[]
+    with open("places.csv",'r',encoding='utf-8-sig') as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            rows.append(row[0].split(";")[2].lower())
+    for x in sentences:
+        for y in x.split(" "):
+            name = get_close_matches(y.lower(),rows, 1, 0.80)
+            if len(name) != 0:
+                answers = [y,rows[math.trunc(random.random()*50)],math.trunc(random.random()*50),math.trunc(random.random()*50)]
+                result.append( {
+                        "question":x.replace(y,"_________"),
+                        "answer":y,
+                        "answers": answers
+                    })
+                break
+    return result
--- backend/quiz.py
+++ backend/quiz.py
@@ -1,5 +1,18 @@
 import os
 import json
+<<<<<<< Updated upstream
+=======
+from difflib import get_close_matches
+import random
+import pdfReader
+import questionGenerator
+import csv
+
+def return_json_response(message, status_code):
+    return {
+        "message": message
+    }, status_code
+>>>>>>> Stashed changes
 
 def read_file_names():
     arr = os.listdir('./pdfs_and_words')
@@ -7,5 +20,53 @@ def read_file_names():
 
 def read_files(requested_subject):
     files = read_file_names()
+<<<<<<< Updated upstream
     print(files)
     return 'test'
+=======
+    searched_files = get_close_matches(requested_subject, files, 2, 0.4)
+    return searched_files
+
+def make_questions(fileText):
+    return 'test'
+
+
+def make_quiz(requested_subject,requested_level):
+    searched_files = read_files(requested_subject)
+    if not searched_files:
+        return return_json_response('Nie znaleziono żadnych danych na podstawie frazy wyszukiwania.', 400)
+    
+    questions = []
+    level = 0
+    max_size = 0 
+
+    if requested_level == 1:
+        level = 100
+        max_size = 5
+    elif requested_level == 2:
+        level = 50
+        max_size = 10
+    else:
+        level = 5
+        max_size = 20
+
+    for fileName in searched_files:
+        fileContent = pdfReader.getTextFromFile('./pdfs_and_words/' + fileName)
+        # method to make questions until it will reach specified amount of questions
+        # make_questions(fileContent)
+        readyQuestions = questionGenerator.generateDate(pdfReader.sliceSentence(fileContent), level)
+        readyQuestions += questionGenerator.generateName(pdfReader.sliceSentence(fileContent),level)
+        rows=[]
+        with open("banList.csv",'r',encoding='utf-8-sig') as csvfile:
+                csvreader = csv.reader(csvfile)
+                for row in csvreader:
+                    rows.append(row[0].lower())
+        for question in readyQuestions:
+            for row in rows:
+                if row not in question['question']:
+                    print(question['question'])
+                    questions.append(question)
+        # filesContent.append(pdfReader.getTextFromFile('./pdfs_and_words/' + fileName))
+    random.shuffle(questions)
+    return questions[:max_size]
+>>>>>>> Stashed changes
